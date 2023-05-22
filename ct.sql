64100c40431f774e1861453bff906fe24671168d
dariustann <131232989+dariustann@users.noreply.github.com>
2023-05-22 10:56:45 +0800

    Add files via upload

---
+++ ct.sql
@@ -0,0 +1,12 @@
+create table info (
+
+	 id INTEGER  primary key AUTOINCREMENT,
+	 username varchar(500),
+	 address varchar(500),
+	 email varchar(500),
+	 phone int not null,
+	 age int not null,
+	 gender varchar(500),
+	 lang varchar(500),
+	 msg varchar(500)
+);
